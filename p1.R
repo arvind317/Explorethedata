7a25986be0dcd6fe54025aa50ed0c47b7d344382
arvind317 <81642506+arvind317@users.noreply.github.com>
2021-04-03 15:33:12 +0530

    thisisit

--- p1.R
+++ p1.R
@@ -1,9 +1,9 @@
 library("data.table")
-setwd("‪C:\Users\arvin\Desktop\coursera\Explorethedata")
+setwd("C:/Users/arvin/Desktop/coursera/Explorethedata")
 party <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 party[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-party[, Det := lapply(.SD, as.Det, "%d/%m/%Y"), .SDcols = c("Det")]
-party <- party[(Det >= "2007-02-01") & (Det < "2007-02-03")]
+party[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+party <- party[(Date >= "2007-02-01") & (Date < "2007-02-03")]
 png("p1.png", width=480, height=480)
 
 hist(party[,Global_active_power], main="Global Active Power", xlab="Global Active Power (kilowatts", ylab="Frequency", col="Red")
